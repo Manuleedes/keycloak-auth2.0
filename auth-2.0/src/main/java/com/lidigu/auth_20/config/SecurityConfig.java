c3bd11753e597ec12faf167166111c29339b2f5c
Manuleedes <manuleedes@gmail.com>
2026-03-19 14:25:17 +0300

    implement keycloak sessions and home controller

--- auth-2.0/src/main/java/com/lidigu/auth_20/config/SecurityConfig.java
+++ auth-2.0/src/main/java/com/lidigu/auth_20/config/SecurityConfig.java
@@ -1,17 +1,29 @@
 package com.lidigu.auth_20.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+     @Autowired
+    private final ClientRegistrationRepository clientRegistrationRepository;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+       OidcClientInitiatedLogoutSuccessHandler handler = new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
+
+       handler.setPostLogoutRedirectUri("{baseUrl}");
+
         http
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/login", "/error").permitAll()
@@ -19,7 +31,10 @@ public class SecurityConfig {
                 .oauth2Login(oauth2 -> oauth2
                         .loginPage("/login")
                         .defaultSuccessUrl("/home", true)
-                );
+                )
+                .logout(logout -> logout
+                        .logoutSuccessHandler(handler))
+        ;
         return http.build();
     }
 }
--- auth-2.0/src/main/java/com/lidigu/auth_20/controller/HomeController.java
+++ auth-2.0/src/main/java/com/lidigu/auth_20/controller/HomeController.java
@@ -1,12 +1,19 @@
 package com.lidigu.auth_20.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
     @GetMapping("/home")
-    public String home(){
+    public String home(@AuthenticationPrincipal OidcUser principal, Model model){
+        model.addAttribute( "username", principal.getPreferredUsername());
+        model.addAttribute( "email", principal.getEmail());
+        model.addAttribute("name", principal.getFullName());
+        model.addAttribute("roles", principal.getAuthorities());
         return "home";
     }
 }
